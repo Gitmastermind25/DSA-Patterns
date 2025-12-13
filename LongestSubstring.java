0424b0006589deca338592bc2de654acc4149506
Yogita <yogitapandit2505@gmail.com>
2025-12-14 01:34:36 +0530

    LongestSubstring.java

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -1,35 +1,25 @@
-import java.util.HashMap;
-
+import java.util.HashSet;
 class LongestSubstring {
 
     public static int lengthOfLongestSubstring(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        int maxLen = 0;
-
-        // Left pointer
-        int left = 0;
+        HashSet<Character> set = new HashSet<>();
+        int left = 0, maxLen = 0;
 
         for (int right = 0; right < s.length(); right++) {
-            char c = s.charAt(right);
-
-            // If character is already in map, move left pointer
-            if (map.containsKey(c)) {
-                // Move left to one position after previous index of current character
-                left = Math.max(left, map.get(c) + 1);
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
             }
-
-            // Update the character's latest index
-            map.put(c, right);
-
-            // Update max length
+            set.add(s.charAt(right));
             maxLen = Math.max(maxLen, right - left + 1);
         }
-
         return maxLen;
     }
 
     public static void main(String[] args) {
+        // Input in code
         String s = "abcabcbb";
+
         int result = lengthOfLongestSubstring(s);
         System.out.println("Longest substring length: " + result);
     }
