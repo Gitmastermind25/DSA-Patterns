d272d8f2e070c4366315def46bdd178b872d442c
Yogita <yogitapandit2505@gmail.com>
2026-01-18 22:02:15 +0530

    53. Maximum Subarray

---
+++ MaximumSubarray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class MaximumSubarray {
+
+    public static int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read entire line of input
+        String[] input = sc.nextLine().trim().split(" ");
+        int[] nums = new int[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            nums[i] = Integer.parseInt(input[i]);
+        }
+
+        // Output result
+        System.out.println(maxSubArray(nums));
+
+        sc.close();
+    }
+}
