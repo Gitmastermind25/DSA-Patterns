877c09aaf7e8136cdb906209ef7deb2812684a39
Yogita <yogitapandit2505@gmail.com>
2026-01-25 23:07:00 +0530

    724. Find Pivot Index

---
+++ PivotIndex.java
@@ -0,0 +1,26 @@
+public class PivotIndex {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 7, 3, 6, 5, 6};
+
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if (leftSum == rightSum) {
+                System.out.println("Pivot Index: " + i);
+                return;
+            }
+
+            leftSum += nums[i];
+        }
+
+        System.out.println("Pivot Index: -1");
+    }
+}
