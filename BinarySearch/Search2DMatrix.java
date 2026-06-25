4368e042fea9e2e9c79f9bf666b68e109c3ae5bc
Yogita Pandit <yogitapandit2505@gmail.com>
2026-06-25 16:04:36 +0530

    Search a 2D Matrix

---
+++ BinarySearch/Search2DMatrix.java
@@ -0,0 +1,42 @@
+package BinarySearch;
+
+public class Search2DMatrix {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int start = 0;
+        int end = m * n - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            int row = mid / n;
+            int col = mid % n;
+
+            if (matrix[row][col] > target) {
+                end = mid - 1;
+            } else if (matrix[row][col] < target) {
+                start = mid + 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60}
+        };
+
+        int target = 3;
+
+        Search2DMatrix obj = new Search2DMatrix();
+        System.out.println(obj.searchMatrix(matrix, target));
+    }
+}
