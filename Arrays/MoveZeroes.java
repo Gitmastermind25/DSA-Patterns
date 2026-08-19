f6da39b1f65f353ddcd032a1db0275cc7df6684f
Yogita Pandit <yogitapandit2505@gmail.com>
2026-08-19 15:05:27 +0530

    MoveZeroes- 283

---
+++ Arrays/MoveZeroes.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class MoveZeroes {
+
+    public static void main(String[] args) {
+
+        int[] nums = {0, 1, 0, 3, 12};
+
+        int i = 0;
+
+        for (int j = 0; j < nums.length; j++) {
+
+            if (nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+
+                i++;
+            }
+        }
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
